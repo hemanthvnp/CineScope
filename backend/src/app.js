8a64ce0131deb3da29b9aac591ca5ed69c0b2a97
Lalitraam <lalitraamskr@gmail.com>
2026-03-02 20:19:44 +0530

    added searchbar watchlist

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const cors = require("cors")
 
+const watchlistRoutes = require("./routes/watchlistRoutes")
+
+
 const movieRoutes = require("./routes/movieRoutes")
 const userRoutes = require("./routes/userRoutes")
 
@@ -8,6 +11,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/watchlist", watchlistRoutes)
 
 app.use("/api/movies", movieRoutes)
 app.use("/api/users", userRoutes)
